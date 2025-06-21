9c821f1c219152c03e89e148c66298e2c69e8050
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-06-21 10:45:38 +0530

    Add MongoDB, Basic Models & Complete Routing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('SkillSwap API'));
+router.use('/users', require('./user'));
+
+router.get('/', (req, res) => res.send('API Home'));
 
 module.exports = router;
