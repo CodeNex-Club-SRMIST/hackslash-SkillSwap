7f5b7b5aac4f860919a693dc577121af8115b447
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-06-22 03:34:10 +0530

    removed the unwanted files

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/users', require('./user'));
-
-router.get('/', (req, res) => res.send('API Home'));
-
-module.exports = router;
